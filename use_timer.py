3c9b00e4a8819d81bb9ddbd66085b2e244b213c7
nic <nwanavit@hatari.cc>
2019-11-16 10:00:43 +0700

    edit function

--- use_timer.py
+++ use_timer.py
@@ -2,27 +2,33 @@ import numpy as np
 import time
 import cupy as cp
 ### Numpy and CPU
-i=700
-def crazyrun():
+def crazyrun(i=700):
+    answer={}
+    answer['numpy']={}
+    answer['cupy']={}
     s = time.time()
     x_cpu = np.ones((i,i,i))
     e = time.time()
     print('numpy generation is {}'.format(e - s))
+    answer['numpy']['generation']=e-s
     ### CuPy and GPU
     s = time.time()
     x_gpu = cp.ones((i,i,i))
     e = time.time()
     print('cupy generation is {}'.format(e - s))
+    answer['cupy']['generation']=e-s
     ### Numpy and CPU
     s = time.time()
     x_cpu *= 5
     e = time.time()
     print('numpy calculation is {}'.format(e - s))
+    answer['numpy']['calculation']=e-s
     ### CuPy and GPU
     s = time.time()
     x_gpu *= 5
     e = time.time()
     print('cupy calculation is {}'.format(e - s))
+    answer['cupy']['calculation']=e-s
     ### Numpy and CPU
     s = time.time()
     x_cpu *= 5
@@ -30,6 +36,7 @@ def crazyrun():
     x_cpu += x_cpu
     e = time.time()
     print('numpy calculation is {}'.format(e - s))
+    answer['numpy']['calculation2']=e-s
     ### CuPy and GPU
     s = time.time()
     x_gpu *= 5
@@ -37,6 +44,9 @@ def crazyrun():
     x_gpu += x_gpu
     e = time.time()
     print('cupy calculation is {}'.format(e - s))
+    answer['cupy']['calculation2']=e-s
+    return answer
 
-for i in range(500):
-    crazyrun()
+if __name__=='__main__':
+    for i in range(500):
+        crazyrun()
